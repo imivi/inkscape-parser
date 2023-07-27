40adf2145736dd44a4152eeb3692afa9581aa9bf
unknown <vitto.m93@gmail.com>
2023-07-27 14:38:40 +0200

    CLI error handling

--- src/cli.ts
+++ src/cli.ts
@@ -3,16 +3,23 @@
 import { parseInkscape } from "./index"
 import fs from "fs"
 
-const inputPath  = process.argv[2]
-const outputPath = process.argv[3]
 
+if(process.argv.length < 4) {
+    console.log("Error: too few arguments provided. Please provide the path to the svg file and an output path (.json)")
+}
 
-const svgText = fs.readFileSync(inputPath, { encoding: "utf-8" })
-
-const { pageProperties, elements } = parseInkscape(svgText)
-
-fs.writeFileSync(
-    outputPath,
-    JSON.stringify({ pageProperties, elements }, null, 4),
-    { encoding: "utf-8" }
-)
+else {
+    const inputPath  = process.argv[2]
+    const outputPath = process.argv[3]
+    
+    
+    const svgText = fs.readFileSync(inputPath, { encoding: "utf-8" })
+    
+    const { pageProperties, elements } = parseInkscape(svgText)
+    
+    fs.writeFileSync(
+        outputPath,
+        JSON.stringify({ pageProperties, elements }, null, 4),
+        { encoding: "utf-8" }
+    )
+}
